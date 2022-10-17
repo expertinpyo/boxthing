77bd4826547ff536f1cfc10831ef56ee14bef81f
expertinpyo <ypd02363@naver.com>
2022-10-17 16:30:26 +0900

    FEAT: Add Backend First Layer

---
+++ back/src/main/java/com/boxthing/BoxthingApplication.java
@@ -0,0 +1,13 @@
+package com.boxthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxthingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxthingApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/boxthing/BoxthingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxthingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

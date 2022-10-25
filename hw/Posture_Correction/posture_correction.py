4a87b058f3a4b0346c79c166070894733dc7862a
SSAFY <jaeung98@naver.com>
2022-10-25 17:32:06 +0900

    FEAT:Update posture correction

--- hw/Posture_Correction/posture_correction.py
+++ hw/Posture_Correction/posture_correction.py
@@ -2,79 +2,89 @@ import cv2, dlib, math
 import cvlib as cv
 import time
 
-def find_distance(img):
-    global min_x, max_x, min_y, max_y
-    #dets = detector(img_gray, 1)
-    faces, confidence = cv.detect_face(image)
+def find_distance(img): # 얼굴 검출 및 거리 반환
+    faces, confidence = cv.detect_face(img)
+    
+    # 검출된 얼굴 없으시 0 반환
+    if len(faces) == 0 :
+        return 0, 0
 
     result_list = []
+    down_point = 0
+    max_area = 0
+    min_x = max_x = min_y = max_y = 0
+    
+    # 가장 가까운 얼굴영역 검출
     for face in faces:
+        area = (face[2]-face[0]) * (face[3]-face[1])
+        if max_area < area :
+            max_area = area
             resulti = dlib.rectangle(face[0], face[1], face[2], face[3])
-            result_list.append(resulti)
+    
+    result_list.append(resulti)
 
-    for k, d in enumerate(result_list):
-        shape = predictor(img_gray, d) #shape: 얼굴 랜드마크 추출
+    #  검출된 영역 내 관자놀이 탐색 후 관자놀이 간 거리를 반환
+    for k, d in enumerate(result_list): 
+        shape = predictor(img, d) #shape: 얼굴 랜드마크 추출 
 
+        p = shape.part(0)
+        #cv2.circle(img, (p.x, p.y), 2, (0, 255, 0), -1)
+        min_x = p.x
+        min_y = p.y
+        p = shape.part(16)
+        #cv2.circle(img, (p.x, p.y), 2, (0, 255, 0), -1)
+        max_x = p.x
+        max_y = p.y
+        dx = max_x - min_x    
+        dy = max_y - min_y 
+        dis = math.sqrt((dx * dx) + (dy * dy))
+    
+    return dis, down_point
 
-        for i in range(shape.num_parts): #총 68개
-            shape_point = shape.part(i)
-            if i == 36 :
-                min_x = shape_point.x
-                min_y = shape_point.y
-            if i == 45 :
-                max_x = shape_point.x
-                max_y = shape_point.y
-
-    dx = max_x - min_x
-    dy = max_y - min_y
-
-    dis = math.sqrt((dx * dx) + (dy * dy))
-
-    return dis
-
-def take_picture():
+def take_picture():  # 사진 찍고 크기 조정
     global webcam
-
+    
     status, frame = webcam.read()
-
+    
     image = cv2.resize(frame, dsize=(640, 480), interpolation=cv2.INTER_AREA)
     #img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+    
     return image
-
+    
 #준비
 predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
 
 webcam = cv2.VideoCapture(0)
 
-min_x = max_x = min_y = max_y = 0
+# 올바른 자세를 1장 찍고 기준점을 삼는다.
 
 for i in range(3):
     print(i+1)
     time.sleep(1)
 
 first_img = take_picture()
-first_dis = find_distance(first_img)
+first_dis, first_down = find_distance(first_img)
 
 print(first_dis)
 
-bad_posture = 0
-
+# 기준과 비교하며 자세판단
 while webcam.isOpened():
     image = take_picture()
-
-    distance = find_distance(image)
-
-    if distance > first_dis + 20 or distance < first_dis - 13 :
-        bad_posture += 1
+    distance, down_point = find_distance(image)
+    
+    if distance > first_dis + 15 :
+        print("잘못된 자세")
+    elif down_point - first_down > 12 :
+        print("허리무리")
+    elif distance == 0 : 
+        print("사람 없음")
     else :
-        if bad_posture > 0 : bad_posture = 0
         print("올바른 자세", distance)
-
-    if bad_posture > 3 : print("잘못된 자세")
-
     time.sleep(1)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 webcam.release()
 cv2.destroyAllWindows()

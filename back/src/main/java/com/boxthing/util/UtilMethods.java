770758f95eed31d36f1bc699bbc0526337de2e8f
inpyo <ypd02363@naver.com>
2022-11-04 14:01:16 +0900

    FEAT : Solve Merge Conflict

--- back/src/main/java/com/boxthing/util/UtilMethods.java
+++ back/src/main/java/com/boxthing/util/UtilMethods.java
@@ -3,7 +3,7 @@ package com.boxthing.util;
 import com.google.gson.Gson;
 import java.lang.reflect.Type;
 
-public class ObjectConvertUtil {
+public class UtilMethods {
   public static <T> T jsonConverter(Object object, Type type) {
     Gson gson = new Gson();
     return gson.fromJson(gson.toJson(object), type);

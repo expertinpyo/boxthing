cbd6d395a4068202e2fb07d0fe7b63f23824dc81
한동현 <handh0519@naver.com>
2022-11-09 09:01:05 +0900

    FIX: fix code to upgrade camera

--- hw/Posture_Correction/posture_correction_test.py
+++ hw/Posture_Correction/posture_correction_test.py
@@ -74,11 +74,16 @@ detector = dlib.get_frontal_face_detector()
 first_img = None
 first_dis = first_down = first_area = 0
 webcam = cv2.VideoCapture(0)
-async def check_pose():   
+
+
+async def check_pose():
     # 0 : 올바른 자세, 1 : 거북목, 2: 허리무리
     send_posture_flag = 0
     posture_score = 0
 
+    posture_cnt = 0
+    
+    
     while True : 
         init()
         if first_dis or first_down or first_area :
@@ -94,32 +99,30 @@ async def check_pose():
             while webcam.isOpened():
                 image = take_picture()
                 now_dis, now_down, now_area = find_distance(image)
-                
-                if now_dis - first_dis > 15 :
-                    send_posture_flag = 2
-                    posture_score = int(87-(now_dis-first_dis)/2)
-                    if posture_score < 60 : posture_score = 60
-                    yield((send_posture_flag, "거북목", posture_score))
-                    #print(send_posture_flag, "거북목", posture_score)
-                elif now_down - first_down > 12 :
-                    send_posture_flag = 3
-                    posture_score = int(80-(now_down - first_down)/4)
-                    if posture_score < 60 : posture_score = 60
-                    yield((send_posture_flag, "허리무리", posture_score))
-                    #print(send_posture_flag, "허리무리", posture_score)
-                elif now_dis == 0 and now_down == 0 and now_area == 0: 
-                    posture_score = -1
-                    send_posture_flag = 4
-                    yield((send_posture_flag, "사람 없음", posture_score))
-                    #print(send_posture_flag, "사람 없음", posture_score)
-                else :
-                    send_posture_flag = 1
-                    if now_dis - first_dis < 0 :
-                        posture_score = int(100+(now_dis - first_dis))
-                    else : 
-                        posture_score = int(100-(now_dis - first_dis))
-                    yield((send_posture_flag, "올바른 자세", posture_score))
-                    #print(send_posture_flag, "올바른 자세", posture_score)
+                if posture_cnt == 5:
+                    if now_dis - first_dis > 15 :
+                        send_posture_flag = 2
+                        posture_score = int(87-(now_dis-first_dis)/2)
+                        if posture_score < 60 : posture_score = 60
+                        yield((send_posture_flag, "거북목", posture_score))
+                    elif now_down - first_down > 12 :
+                        send_posture_flag = 3
+                        posture_score = int(80-(now_down - first_down)/4)
+                        if posture_score < 60 : posture_score = 60
+                        yield((send_posture_flag, "허리무리", posture_score))
+                    elif now_dis == 0 and now_down == 0 and now_area == 0: 
+                        posture_score = -1
+                        send_posture_flag = 4
+                        yield((send_posture_flag, "사람 없음", posture_score))
+                    else :
+                        send_posture_flag = 1
+                        if now_dis - first_dis < 0 :
+                            posture_score = int(100+(now_dis - first_dis))
+                        else : 
+                            posture_score = int(100-(now_dis - first_dis))
+                        yield((send_posture_flag, "올바른 자세", posture_score))
+                    posture_cnt = 0
+                posture_cnt += 1
                 await asyncio.sleep(0.2)
                 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
